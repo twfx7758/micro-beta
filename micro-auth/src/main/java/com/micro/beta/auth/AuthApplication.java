fd6db95f47cb7ce78d1c698763b16696f988c785
twfx7 <twfx7758@163.com>
2018-07-25 17:02:51 +0800

    微服务项目测试

---
+++ micro-auth/src/main/java/com/micro/beta/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)AuthApplication.java      2018/7/25
+ *
+ * 百得利集团拥有完全的版权
+ * 使用者必须经过许可
+ */
+package com.micro.beta.auth;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author twfx7
+ * @since JDK 1.8
+ */
+public class AuthApplication {
+}
