3c5e63a8443fc03d65ecd3e66cbbebdb86fad037
twfx7 <twfx7758@163.com>
2018-07-26 10:58:12 +0800

    微服务项目测试

--- micro-auth/src/main/java/com/micro/beta/auth/AuthApplication.java
+++ micro-auth/src/main/java/com/micro/beta/auth/AuthApplication.java
@@ -18,7 +18,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @since JDK 1.8
  */
 @SpringBootApplication
-@EnableAuthorizationServer
 public class AuthApplication {
     public static void main(String args[]) {
         SpringApplication.run(AuthApplication.class, args);
---
+++ micro-auth/src/main/java/com/micro/beta/auth/controller/TestEndpoints.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)TestEndpoints.java      2018/7/26
+ *
+ * 百得利集团拥有完全的版权
+ * 使用者必须经过许可
+ */
+package com.micro.beta.auth.controller;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author twfx7
+ * @since JDK 1.8
+ */
+public class TestEndpoints {
+}
