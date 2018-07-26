5ea4cdf2839bdf2201f437a4d8c468c7f8598d82
twfx7 <twfx7758@163.com>
2018-07-26 11:47:59 +0800

    OAuth2功能测试

---
+++ micro-auth/src/main/java/com/micro/beta/auth/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+/*
+ * @(#)SecurityConfiguration.java      2018/7/26
+ *
+ * 百得利集团拥有完全的版权
+ * 使用者必须经过许可
+ */
+package com.micro.beta.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * 功能：spring security配置
+ *
+ * @author twfx7
+ * @since JDK 1.8
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password("123456").authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password("123456").authorities("USER").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers().anyRequest()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/*").permitAll();
+    }
+}
