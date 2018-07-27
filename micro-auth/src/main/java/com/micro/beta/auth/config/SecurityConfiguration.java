ec586a17d489d9ca7b02f064d06d10dac8486ed9
twfx7 <twfx7758@163.com>
2018-07-27 15:21:16 +0800

    OAuth2功能测试

--- micro-auth/src/main/java/com/micro/beta/auth/config/SecurityConfiguration.java
+++ micro-auth/src/main/java/com/micro/beta/auth/config/SecurityConfiguration.java
@@ -7,13 +7,16 @@
 package com.micro.beta.auth.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 /**
  * 功能：spring security配置
@@ -27,10 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     @Override
     protected UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user_1").password("123456").authorities("USER").build());
-        manager.createUser(User.withUsername("user_2").password("123456").authorities("USER").build());
-        return manager;
+        return this.getUserDetailsService("mysql");
     }
 
     @Override
@@ -40,5 +40,45 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/oauth/*").permitAll();
+    }
+
+    private UserDetailsService getUserDetailsService(String storeType) {
+        UserDetailsService userDetailsService = null;
+        switch (storeType) {
+            case "memory":
+                userDetailsService = this.storeUserDetailsInMemory();
+                break;
+            case "mysql":
+                userDetailsService = this.storeUserDetailsInMySQL();
+                break;
+            default:
+                userDetailsService = this.storeUserDetailsInMemory();
+                break;
+        }
+        return userDetailsService;
+    }
+    //用户信息存储在内存中
+    private UserDetailsService storeUserDetailsInMemory() {
+        final String pwd = "123456";
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password(pwd).authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password(pwd).authorities("USER").build());
+        return manager;
+    }
+    //用户信息存储在mysql里
+    private UserDetailsService storeUserDetailsInMySQL() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+        manager.setDataSource(dataSource());
+        return manager;
+    }
+    //获取mysql的数据源
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://bdlsaledbdev1.xbotech.com:3306/oauth?useUnicode=true&characterEncoding=utf-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("BdlDevl123!");
+
+        return dataSource;
     }
 }
