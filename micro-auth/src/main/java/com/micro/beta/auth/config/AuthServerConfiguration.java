3e2e81f2717be40c57780fb56ec1910a760a48ee
twfx7 <twfx7758@163.com>
2018-07-31 09:09:25 +0800

    OAuth2功能测试

--- micro-auth/src/main/java/com/micro/beta/auth/config/AuthServerConfiguration.java
+++ micro-auth/src/main/java/com/micro/beta/auth/config/AuthServerConfiguration.java
@@ -9,6 +9,7 @@ package com.micro.beta.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,6 +21,7 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
@@ -33,20 +35,30 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
+    private static final String DEMO_RESOURCE_ID = "*";
+
     @Autowired
     AuthenticationManager authenticationManager;
 
     @Autowired
     DataSource dataSource;
 
-    @Bean
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    @Autowired
     public TokenStore tokenStore() {
         return new JdbcTokenStore(dataSource);
     }
 
-    @Bean
+    @Autowired
     public ClientDetailsService clientDetails() {
         return new JdbcClientDetailsService(dataSource);
+    }
+
+    @Autowired
+    private TokenStore getRedisTokenStore() {
+        return new RedisTokenStore(redisConnectionFactory);
     }
 
     @Override
