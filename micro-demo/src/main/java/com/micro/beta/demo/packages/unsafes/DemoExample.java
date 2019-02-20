ee80c94f965a9ddccf7554c7ad8c3037c6de4a53
屈文斌 <quwenbin@blchina.com>
2019-02-20 13:32:54 +0800

    测试unsafe包

---
+++ micro-demo/src/main/java/com/micro/beta/demo/packages/unsafes/DemoExample.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)DemoExample.java      2019/2/20
+ *
+ * 百得利集团拥有完全的版权
+ * 使用者必须经过许可
+ */
+package com.micro.beta.demo.packages.unsafes;
+
+/**
+ * 功能：
+ *
+ * @author twfx7
+ * @since JDK 1.8
+ */
+public class DemoExample {
+
+}
---
+++ micro-demo/src/main/java/com/micro/beta/demo/packages/unsafes/DemoUnsafe.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)DemoUnsafe.java      2019/2/20
+ *
+ * 百得利集团拥有完全的版权
+ * 使用者必须经过许可
+ */
+package com.micro.beta.demo.packages.unsafes;
+
+/**
+ * 功能：unsafe包测试示例
+ *
+ * @author twfx7
+ * @since JDK 1.8
+ */
+public class DemoUnsafe {
+    public static void main(String args[]) {
+        DemoExample example = new DemoExample();
+    }
+}
