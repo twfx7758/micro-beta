b167fcc8760ead36770a1ec5a1e5df1f5023ef25
屈文斌 <quwenbin@blchina.com>
2019-04-25 10:51:19 +0800

    阿里开源分布式事务-seata测试程序

---
+++ micro-seata/seata-storage/src/main/java/com/micro/beta/seata/storage/StorageApplication.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)StorageApplication.java      2019/4/25
+ *
+ * 百得利集团拥有完全的版权
+ * 使用者必须经过许可
+ */
+package com.micro.beta.seata.storage;
+
+/**
+ * 功能：stroage微服务
+ *
+ * @author twfx7
+ * @since JDK 1.8
+ */
+public class StorageApplication {
+    public static void main(String[] args) {
+
+    }
+}
